7ae9084e1847415f047ae610ddb42cf475726d4b
Tom Gibbons <tgibbons@css.edu>
2018-09-17 21:19:28 -0500

    Second Commit
    Added a cube and a material.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    //private float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float moveHoizental = Input.GetAxis("Horizontal");
+        float moveVerticle = Input.GetAxis("Vertical");
+        Vector3 movment = new Vector3(moveHoizental, 0.0f, moveVerticle);
+        rb.AddForce(movment * 10);
+    }
+}
